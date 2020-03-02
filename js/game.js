589f4cc3f89ca7641238da9db7003c60cd969ff9
roshak7 <60941581+roshak7@users.noreply.github.com>
2020-03-03 00:39:09 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -5,21 +5,19 @@ let hits = 0;
 let firstHitTime = 0;
 
 function round() {
-  // FIXME: надо бы убрать "target" прежде чем искать новый
+  $('.target').removeClass('target');
+  $('.miss').removeClass('.miss');
 
   let divSelector = randomDivId();
   $(divSelector).addClass("target");
-  // TODO: помечать target текущим номером
+  $(divSelector).text(hits + 1);
 
-  // FIXME: тут надо определять при первом клике firstHitTime
-
-  if (hits === maxHits) {
-    endGame();
+  if (hits === 1) {firstHitTime = getTimestamp();}
+  if (hits===maxHits) {endGame();}
   }
-}
 
 function endGame() {
-  // FIXME: спрятать игровое поле сначала
+  $('.game-field').hide();
 
   let totalPlayedMillis = getTimestamp() - firstHitTime;
   let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
@@ -29,16 +27,15 @@ function endGame() {
 }
 
 function handleClick(event) {
-  // FIXME: убирать текст со старых таргетов. Кажется есть .text?
+  let target = $(event.target);
   if ($(event.target).hasClass("target")) {
     hits = hits + 1;
+    target.text();
     round();
-  }
-  // TODO: как-то отмечать если мы промахнулись? См CSS класс .miss
+  } else {$(event.target).addClass('miss'); }
 }
 
 function init() {
-  // TODO: заказчик просил отдельную кнопку, запускающую игру а не просто по загрузке
   round();
 
   $(".game-field").click(handleClick);

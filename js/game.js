0aca4d0c881bcb8daff4f445e7a1113936605526
roshak7 <60941581+roshak7@users.noreply.github.com>
2020-03-04 01:01:33 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -5,42 +5,42 @@ let hits = 0;
 let firstHitTime = 0;
 
 function round() {
-  $('.target').removeClass('target');
-  $('.miss').removeClass('.miss');
+  $('.target').removeClass('target'); 
+  $('.miss').removeClass('miss'); 
 
   let divSelector = randomDivId();
-  $(divSelector).addClass("target");
-  $(divSelector).text(hits + 1);
+  $(divSelector).addClass('target')
+  $(divSelector).text(hits + 1)
 
-  if (hits === 1) {firstHitTime = getTimestamp();}
-  if (hits===maxHits) {endGame();}
-  }
+  if (hits === 1) { firstHitTime = getTimestamp(); } 
+  if (hits === maxHits) { endGame(); } 
+}
 
 function endGame() {
   $('.game-field').hide();
 
   let totalPlayedMillis = getTimestamp() - firstHitTime;
   let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
-  $("#total-time-played").text(totalPlayedSeconds);
+  $('#total-time-played').text(totalPlayedSeconds);
 
-  $("#win-message").removeClass("d-none");
+  $('#win-message').removeClass("d-none");
 }
 
 function handleClick(event) {
-  let target = $(event.target);
-  if ($(event.target).hasClass("target")) {
+  let target = $(event.target)
+  if (target.hasClass('target')) {
     hits = hits + 1;
-    target.text();
+    target.text('');
     round();
-  } else {$(event.target).addClass('miss'); }
+  } else { $(event.target).addClass('miss'); } 
 }
 
 function init() {
-  round();
+    round();
 
-  $(".game-field").click(handleClick);
-  $("#button-reload").click(function() {
-    location.reload();
+    $('.game-field').click(handleClick);
+    $('#button-reload').click(function() {
+      location.reload();
   });
 }
 
